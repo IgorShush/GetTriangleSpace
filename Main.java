2b00f887a90403daa408345455aad46589fdf522
Igor <36569562+IgorShush@users.noreply.github.com>
2019-03-05 01:53:43 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        Triangle abc = new Triangle(new Point(3, 3), new Point(0, 3), new Point(0, 0));
+        System.out.println(abc.getSpace());
+    }
+}
